bdfc637bfd6cca563864edf509eba362790c3efb
agustina.hernandez <agustina.hernandez@eva.edu.uy>
2022-07-28 10:41:27 -0300

    Revert "monthly billing screen"
    This reverts commit 6b90067f5384a07eee8548b397cf0d9f2fea5f4e.

--- pages/invoices/index.js
+++ pages/invoices/index.js
@@ -2,11 +2,8 @@ import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import Layout from "../../components/layout";
 import Sidebar from "../../components/sidebar";
-import { useState } from "react";
 
 export default function Invoice() {
-  const [loader, setLoader] = useState(false);
-  const [message, setMessage] = useState("");
   return (
     <section className="md:m-10 xs:m-2 w-10/12">
       <Head>
@@ -14,47 +11,31 @@ export default function Invoice() {
         <link rel="icon" href="/taskforce-next-admin/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="text-5xl font-bold mt-9">Monthly Billing</h1>
-        <h2 className="text-xl flex font-semibold mt-9">
-          1. Review all records in
-          <a href="https://app.clockify.me/reports/summary">
-            <h2 className="ml-2 text-blue underline font-sans text-xl">
-              clockify.me
-            </h2>
-          </a>
-        </h2>
-        <h2 className="text-xl flex font-semibold mt-9">
-          2. Send invoices to stripe
-        </h2>
-        <div className="md:flex">
-          <form className="flex flex-col mt-10 bg-white max-w-md xs:w-full w-5/12 p-3">
-            <label className="text-xl font-semibold">Period Start</label>
-            <input className="border w-full h-9 mt-4" required />
-          </form>
-          <form className="ml-2 flex flex-col mt-10 bg-white max-w-md xs:w-full w-9/12 p-3">
-            <label className="text-xl font-semibold">Period End</label>
-            <input className="border w-full h-9 mt-4" required />
-          </form>
-        </div>
-        {loader && <div className={styles.loader} />}
-        <button
-          className="p-1.5 rounded text-white h-9 bg-indigo mt-4"
-          type="submit"
-        >
-          Create Invoices
-        </button>
-        {message && (
-          <>
-            <p className="w-full flex justify-center my-4">{message}</p>
-            {keys && (
-              <p className="w-full break-all flex justify-center">{keys}</p>
-            )}
-          </>
-        )}
-        <h2 className="text-xl flex font-semibold mt-9">
-          3. Review all invoices and send
-        </h2>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Generate Invoices from Clockify Time Records
+        </h1>
+
+        <ul>
+          <li>
+            Need a setting page that allows input of api keys that get stored in
+            browser localstorage
+          </li>
+          <li>
+            This page needs a date range selector (from - to) and defaults to
+            last month
+          </li>
+          <li>
+            When date is selected use the clockify api to print each client,
+            each project, sum of hours for the selected date range, and has a
+            checkbox the user can select (all on by default)
+          </li>
+          <li>
+            User checks off checkboxes next to each client, hits [generte
+            invoices], and code sends a request to pipedream for each client for
+            invoice generation
+          </li>
+        </ul>
       </main>
     </section>
   );
